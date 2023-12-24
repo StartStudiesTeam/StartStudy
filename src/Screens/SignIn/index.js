7d4bffef6707df6f9915422f54cab7b9f6a0730b
samuel-jesus <samuel.jesan2018@gmail.com>
2023-12-24 10:34:57 -0300

    feat: created screen sign in defined layout and styles for compopenets

--- src/Screens/SignIn/index.js
+++ src/Screens/SignIn/index.js
@@ -1,30 +1,56 @@
-import React from "react";
-import { Input, Box, Text, Button, ScrollView, Link, Pressable } from "native-base";
+import { React, useState } from "react";
+import { useNavigation } from '@react-navigation/native';
+import { Input, Box, Text, Button, Link, Pressable, FormControl } from "native-base";
 import Icon from 'react-native-vector-icons/FontAwesome';
 import styleSignIn from "./style";
 
 export default function SignIn() {
+    const [showPassword, setShowPassword] = useState(false);
+    const { navigate, goBack } = useNavigation();
+
+    async function goToScreen() {
+        navigate('SignUp');
+    }
+
     return (
-        <Box flex={1} bg="#B6B1B1" alignItems="center" justifyContent="center">
-            <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={styleSignIn.scrollContainer}>
-
-                <Box style={styleSignIn.container}>
-                    <Text style={styleSignIn.labelInput}>Email</Text>
-                    <Input style={styleSignIn.input} variant="filled" placeholder="Type your Email" />
-
-                    <Text style={styleSignIn.labelInput}>Password</Text>
-                    <Input style={styleSignIn.input} variant="filled" placeholder="Type your Password" />
-                    
-                    <Box style={styleSignIn.linkSignIn}>
-                        <Link href="#"><Text style={styleSignIn.linkSignIn} >Já tem conta, fazer login?</Text></Link>
+        <Box style={styleSignIn.container} >
+            <Box style={styleSignIn.content}>
+                <Box style={styleSignIn.brand}></Box>
+                <Box style={styleSignIn.formContent}>
+                    <Box style={styleSignIn.form} >
+                        <FormControl isInvalid={false} style={styleSignIn.formControl}>
+                            <FormControl.Label style={styleSignIn.labelInput} >Email</FormControl.Label>
+                            <Input
+                                style={styleSignIn.input}
+                                variant="filled"
+                                placeholder="Type your Email"
+                                InputLeftElement={<Icon name="at" size={20} color="#ccc" style={{ marginLeft: 12, marginRight: 12 }} />} />
+                            <FormControl.ErrorMessage >
+                                Try different from previous passwords.
+                            </FormControl.ErrorMessage>
+                        </FormControl>
+                        <FormControl isInvalid={false} style={styleSignIn.formControl}>
+                            <FormControl.Label style={styleSignIn.labelInput}>Password</FormControl.Label>
+                            <Input
+                                style={styleSignIn.input}
+                                variant="filled"
+                                type={showPassword ? "text" : "password"}
+                                placeholder="Type your Password"
+                                InputLeftElement={<Icon name="lock" size={20} color="#ccc" style={{ marginLeft: 12, marginRight: 12 }} />}
+                                InputRightElement={<Pressable onPress={() => setShowPassword(!showPassword)}>
+                                    <Icon name={showPassword ? "eye" : "eye-slash"} size={20} color="#ccc" style={{ marginLeft: 12, marginRight: 12 }} />
+                                </Pressable>} />
+                            <FormControl.ErrorMessage >
+                                Try different from previous passwords.
+                            </FormControl.ErrorMessage>
+                        </FormControl>
+                        <Link style={styleSignIn.link} href="#" onPress={() => goToScreen()}>
+                            Already have an account, sign in?
+                        </Link>
                     </Box>
+                    <Button onPress={() => Authenticate()} style={styleSignIn.button}>Sign In</Button>
                 </Box>
-                
-                <Box >
-                    <Button style={styleSignIn.button}>SIGN IN</Button>
-                </Box>
-
-            </ScrollView>
+            </Box>
         </Box>
     )
 }
