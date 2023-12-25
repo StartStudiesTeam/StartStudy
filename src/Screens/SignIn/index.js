0541aa70b27a59a611c544b558d6aab5ec902f61
samuel-jesus <samuel.jesan2018@gmail.com>
2023-12-25 00:28:15 -0300

    feat: implemented validation fields in form with Yup

--- src/Screens/SignIn/index.js
+++ src/Screens/SignIn/index.js
@@ -2,6 +2,7 @@ import { React, useState } from "react";
 import { useNavigation } from '@react-navigation/native';
 import { Input, Box, Text, Button, Link, Pressable, FormControl } from "native-base";
 import Icon from 'react-native-vector-icons/FontAwesome';
+import * as yup from 'yup'
 import { Formik } from "formik";
 import styleSignIn from "./style";
 
@@ -18,13 +19,23 @@ export default function SignIn() {
             <Formik
                 initialValues={{ email: '', password: '' }}
                 onSubmit={values => console.log('Valores do formulário:', values)}
+                validationSchema={yup.object().shape({
+                    email: yup
+                        .string()
+                        .email('Invalid email address format')
+                        .required('Email is required'),
+                    password: yup
+                        .string()
+                        .min(8, 'Password should be at least 8 chars long.')
+                        .required('Password is required'),
+                })}
             >
                 {({ handleChange, handleBlur, handleSubmit, setFieldTouched, touched, isValid, errors, values }) => (
                     <Box style={styleSignIn.content}>
                         <Box style={styleSignIn.brand}></Box>
                         <Box style={styleSignIn.formContent}>
                             <Box style={styleSignIn.form} >
-                                <FormControl isInvalid={false} style={styleSignIn.formControl}>
+                                <FormControl isInvalid={!isValid} style={styleSignIn.formControl}>
                                     <FormControl.Label style={styleSignIn.labelInput} >Email</FormControl.Label>
                                     <Input
                                         value={values.email}
@@ -35,13 +46,18 @@ export default function SignIn() {
                                         placeholder="Type your Email"
                                         InputLeftElement={<Icon name="at" size={20} color="#ccc" style={{ marginLeft: 12, marginRight: 12 }} />} />
                                     {touched.email && errors.email &&
-                                        <Text style={{ fontSize: 12, color: '#FF0D10' }}>{errors.email}</Text>
+                                        <FormControl.ErrorMessage >
+                                            <Text style={{ fontSize: 12, color: '#FF0D10' }}>{errors.email}</Text>
+                                        </FormControl.ErrorMessage>
                                     }
 
                                 </FormControl>
-                                <FormControl isInvalid={false} style={styleSignIn.formControl}>
+                                <FormControl isInvalid={!isValid} style={styleSignIn.formControl}>
                                     <FormControl.Label style={styleSignIn.labelInput}>Password</FormControl.Label>
                                     <Input
+                                        value={values.password}
+                                        onChangeText={handleChange('password')}
+                                        onBlur={() => setFieldTouched('password')}
                                         style={styleSignIn.input}
                                         variant="filled"
                                         type={showPassword ? "text" : "password"}
@@ -52,14 +68,14 @@ export default function SignIn() {
                                                 <Icon name={showPassword ? "eye" : "eye-slash"} size={20} color="#ccc" style={{ marginLeft: 12, marginRight: 12 }} />
                                             </Pressable>} />
                                     <FormControl.ErrorMessage >
-                                        Try different from previous passwords.
+                                        {errors.password}
                                     </FormControl.ErrorMessage>
                                 </FormControl>
                                 <Link style={styleSignIn.link} href="#" onPress={() => goToScreen()}>
                                     Already have an account, sign in?
                                 </Link>
                             </Box>
-                            <Button onPress={handleSubmit} style={styleSignIn.button} disabled={!isValid}>Sign In</Button>
+                            <Button onPress={handleSubmit} style={styleSignIn.button} isDisabled={!isValid}>Sign In</Button>
                         </Box>
                     </Box>
                 )}
