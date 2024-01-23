26f7dcd1f7d75370a1bc16c320d7a1db1a292e3c
Brian Lucca <brianlucca2002@gmail.com>
2024-01-23 00:48:28 -0300

    feat: STASTU-89 added formik and yup for validation on the recovery password screen.

--- src/Screens/RecoveryPassword/index.js
+++ src/Screens/RecoveryPassword/index.js
@@ -1,9 +1,12 @@
 import React, {useState} from 'react';
 import { Input, Box, Text, Button,Link, Spinner,Pressable,FormControl} from "native-base";
 import { useNavigation } from '@react-navigation/native';
+import { Formik } from "formik";
+import * as yup from 'yup'
 import Icon from 'react-native-vector-icons/FontAwesome';
-import Snackbar from 'react-native-snackbar';
 import styleRecoveryPassword from "./styles";
+import api from "../../Services/api";
+
 
 export default function ConfirmEmail() {
 
@@ -11,68 +14,70 @@ export default function ConfirmEmail() {
   const [loading, setIsLoanding] = useState(false);
   const [isPassword, setIsPassword] = useState(true)
 
-  async function newPassword() {
-    setIsLoanding(true),
-    setTimeout(( )=>{
-      navigate('SignIn');
-      setIsLoanding(false);
-    },2000)
-    Snackbar.show({
-        text: `Senha alterada com sucesso!`,
-        duration: Snackbar.LENGTH_LONG,
-        backgroundColor: '#14cf43',
-        textColor: '#fff',
-      });
-  }
   
   async function goToSignIn() {
   navigate('SignIn');
   }
 
+  
   return (
-    <Box style={styleRecoveryPassword.contarinerP}> 
-      <Box style={styleRecoveryPassword.boxImage}></Box>
-
-    <Box style={{marginLeft:16,marginRight:16}}>
-      <Text style={styleRecoveryPassword.labelInput}>New Password</Text>
-
-
-    <Box style={styleRecoveryPassword.container}>
-    <FormControl>
-        <Input
-            style={styleRecoveryPassword.input}
-            variant="filled"
-            placeholder="Type your new password"
-            secureTextEntry={isPassword}
-            InputLeftElement={<Icon style={{marginLeft:17}}name="lock" size={20} color="#ccc"/>}
-            InputRightElement={<Pressable onPress={() => setIsPassword(!isPassword)}>
-            {isPassword == true ?
-            <Icon style={{marginRight:14}} name="eye-slash" size={20} color="#000" />
-            :
-            <Icon style={{marginRight:14}} name="eye" size={20} color="#ccc" />
-            }
-        </Pressable>}
-        />
-        </FormControl>
-    </Box>
-
-    <Link style={styleRecoveryPassword.linkSignIn} 
-    onPress={() => goToSignIn()}> 
-    Return and sign in?</Link>
-    </Box>
-
-      <Box style={styleRecoveryPassword.boxBotao}>
-        <Button 
-        onPress={() => newPassword()}
-        style={styleRecoveryPassword.button}>
-        {loading ? (
-          <Spinner color={'cyan.500'} />
-        ) : (
-          <Text style={styleRecoveryPassword.textBotao}>SAVE</Text>
+    <Box style={styleRecoveryPassword.contarinerP}>
+      <Formik
+          initialValues={{newPassword: ''}}
+          validationSchema={yup.object().shape({
+            newPassword: yup
+              .string()
+              .min(8, 'Password should be at least 8 chars long.')
+              .required('Password is required'),
+          })}
+      >
+        {({ handleChange, handleSubmit, setFieldTouched, touched, isValid, errors, values }) =>(
+          <Box style={styleRecoveryPassword.contarinerP}>
+            <Box style={styleRecoveryPassword.boxImage}></Box>
+            <Box >
+              <FormControl isInvalid={!isValid} style={styleRecoveryPassword.container}>
+                <FormControl.Label style={styleRecoveryPassword.labelInput}>New Password</FormControl.Label>
+                <Input
+                  style={styleRecoveryPassword.input}
+                  value={values.newPassword}
+                  onChangeText={(text) => {
+                    handleChange('newPassword')(text)
+                    setFieldTouched('newPassword', true)
+                  }}
+                  onBlur={() => setFieldTouched('newPassword')}
+                  variant='filled'
+                  placeholder='Type your new Password'
+                  secureTextEntry={isPassword}
+                  InputLeftElement={<Icon style={{marginLeft:17}}name="lock" size={20} color="#ccc"/>}
+                  InputRightElement={
+                  <Pressable onPress={() => setIsPassword(!isPassword)}>
+                    <Icon name={isPassword ? "eye" : "eye-slash"} size={20} color="#ccc" style={{ marginLeft: 12, marginRight: 12 }} />
+                  </Pressable>} />
+                  {touched.newPassword && errors.newPassword && 
+                    <FormControl.ErrorMessage>
+                      <Text>{errors.newPassword}</Text>
+                    </FormControl.ErrorMessage>
+                  }
+              </FormControl>
+              <Link
+              style={styleRecoveryPassword.linkSignIn}
+              onPress={() => goToSignIn()}>
+                Return and sign in?
+              </Link>
+            </Box>
+            <Box style={styleRecoveryPassword.boxBotao}>
+              <Button
+              onPress={() => handleSubmit()}>
+              {loading ? (
+                <Spinner color={'cyan.500'} />
+              ) : (
+                <Text>Save</Text>
+              )}
+              </Button>
+            </Box>
+          </Box>
         )}
-        </Button>
-      </Box>
-
+      </Formik>
     </Box>
   );
 }
