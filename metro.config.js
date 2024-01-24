4b497fa54294ed244eced02ef9c661af5e001730
Anderson Fernandes <anderson.fernandes0402@gmail.com>
2024-01-24 19:55:42 -0300

    fix: correction

--- metro.config.js
+++ metro.config.js
@@ -2,21 +2,6 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
 /** @type {import('expo/metro-config').MetroConfig} */
-module.exports = (async () => {
-    const config = await getDefaultConfig(__dirname);
-  
-    const { transformer, resolver } = config;
-  
-    config.transformer = {
-      ...transformer,
-      babelTransformerPath: require.resolve('react-native-svg-transformer'),
-      ...transformer,
-    };
-    config.resolver = {
-      ...resolver,
-      assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
-      sourceExts: [...resolver.sourceExts, 'svg'],
-    };
-  
-    return config;
-  })();
+const config = getDefaultConfig(__dirname);
+
+module.exports = config;
