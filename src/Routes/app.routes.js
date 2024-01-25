19dfa14166f8ea35a58c062ce3924f6ec856f064
Brian Lucca <brianlucca2002@gmail.com>
2024-01-25 20:39:01 -0300

    fix: resolved conflict.

--- src/Routes/app.routes.js
+++ src/Routes/app.routes.js
@@ -12,7 +12,7 @@ const AppStack = createNativeStackNavigator();
 
 function AppRoutes() {
   return (
-    <AppStack.Navigator initialRouteName="CodeConfirm">
+    <AppStack.Navigator initialRouteName="SignIn">
       <AppStack.Screen
         name="ConfirmEmail"
         component={ConfirmEmail}
