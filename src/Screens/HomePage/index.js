5bb156962fe0d63f0e3c435b4b5d785f9532dc4a
Brian Lucca <brianlucca2002@gmail.com>
2024-01-28 17:53:03 -0300

    feat: STASTU-91 incorporation of figma design into the homePage screen.

--- src/Screens/HomePage/index.js
+++ src/Screens/HomePage/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { Box, Text, Button, ScrollView, Input, FormControl, Pressable } from "native-base";
+import { Box, Text, Button, ScrollView, Input, FormControl, Pressable} from "native-base";
 import { Animated } from 'react-native';
+import { Image } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import styleHomePage from "./style";
 
@@ -15,8 +16,9 @@ export default function HomePage() {
   const popIn = () => {
     setPop(true)
     Animated.timing(icon_1, {
-      toValue: 120,
+      toValue: 115,
       right: 20,
+      left: 30,
       duration: 500,
       useNativeDriver: false,
     }).start()
@@ -61,47 +63,153 @@ export default function HomePage() {
     }).start()
   }
 
+
   return (
-    <Box flex={1} alignItems="center" justifyContent="center">
+    <Box flex={1} alignItems="center" justifyContent="center" style={styleHomePage.container}>
       <ScrollView showsVerticalScrollIndicator={false}>
         <Box>
-          <Box style={styleHomePage.topMenu}>
-            <Box>
-              <Icon name="angle-left" style={styleHomePage.iconAngleLeft} />
-              <Text style={styleHomePage.titleHome}>Home Page</Text>
-              <Icon name="sign-out" style={styleHomePage.iconSignOut} />
+          <Box style={styleHomePage.containerTitle} borderRadius={8}>
+            <Box style={styleHomePage.titleStartStudy}>
+              <Image source={require('../../Assets/BrandTest2.png')} style={styleHomePage.image} />
+              <Text style={[styleHomePage.titleAbout, styleHomePage.titleAboutOne]}>SUA TRILHA DO CONHECIMENTO</Text>
+              <Text style={[styleHomePage.titleAbout, styleHomePage.titleAboutSecond]}>START STUDY</Text>
             </Box>
-          </Box>
-          <Box style={styleHomePage.containerTitle}>
-            <Text style={styleHomePage.titleStartStudy}>START STUDY</Text>
-            <Text style={styleHomePage.titleStartStudy}>SUA TRILHA DO CONHECIMENTO</Text>
           </Box>
           <Box style={styleHomePage.containerSearch}>
             <FormControl>
               <Input
-                placeholder="Search your trail"
                 width={300}
+                height={44}
                 borderRadius={8}
                 style={styleHomePage.Search}
               />
             </FormControl>
-            <Button style={styleHomePage.buttonSearch}><Icon name="search" /></Button>
+            <Button style={styleHomePage.buttonSearch}><Icon name="search" size={17}  /></Button>
           </Box>
           <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
             <Box style={styleHomePage.containerTopics}>
-              <Box style={styleHomePage.topic}><Text style={styleHomePage.textTopic}>Start Study</Text></Box>
-              <Box style={styleHomePage.topic}><Text style={styleHomePage.textTopic}>Start Study</Text></Box>
-              <Box style={styleHomePage.topic}><Text style={styleHomePage.textTopic}>Start Study</Text></Box>
-              <Box style={styleHomePage.topic}><Text style={styleHomePage.textTopic}>Start Study</Text></Box>
+              <Box style={styleHomePage.topic} backgroundColor={'#CBDCBD'}><Text style={styleHomePage.textTopic}>Português</Text></Box>
+              <Box style={styleHomePage.topic} backgroundColor={'#F0E3C9'}><Text style={styleHomePage.textTopic}>Matemática</Text></Box>
+              <Box style={styleHomePage.topic} backgroundColor={'#B1D5DE'}><Text style={styleHomePage.textTopic}>Física</Text></Box>
+              <Box style={styleHomePage.topic} backgroundColor={'#4DA99C'}><Text style={styleHomePage.textTopic}>Programação</Text></Box>
             </Box>
           </ScrollView>
           <ScrollView showsVerticalScrollIndicator={false}>
             <Box style={styleHomePage.containerTrail}>
-              <Box style={styleHomePage.trail}></Box>
-              <Box style={styleHomePage.trail}></Box>
-              <Box style={styleHomePage.trail}></Box>
-              <Box style={styleHomePage.trail}></Box>
-              <Box style={styleHomePage.trail}></Box>
+              <Box style={styleHomePage.cardtrail}>
+                <Box style={styleHomePage.containerTrackTrail}>
+                  <Text style={styleHomePage.nameTrail}>Title Track Here</Text>
+                  <Box style={styleHomePage.userTrack}>
+                    <Box style={styleHomePage.photoProfile}></Box>
+                    <Text style={styleHomePage.nameUser}>@nick_name_here</Text>
+                  </Box>
+                  <Box style={styleHomePage.iconArrowTrail}><Icon name="hand-o-right" size={20} /></Box>
+                </Box>
+                <Box style={styleHomePage.containerAboutTrail}>
+                  <Box>
+                    <Text style={styleHomePage.aboutTitleName}>Coliseu</Text>
+                    <Icon name="file-text-o" size={17} style={styleHomePage.iconFIleAbout}/>
+                    <Icon name="circle" style={{fontSize: 6, top: -10, color: '#0B2C37BF'}} /><Text style={styleHomePage.listAbout}>Cretense e Micênicos</Text>
+                    <Icon name="circle" style={{fontSize: 6, top: -10, color: '#0B2C37BF'}} /><Text style={styleHomePage.listAbout}>Os povos gregos</Text>
+                    <Text style={styleHomePage.sumaryAbout}>A construção começou sob o governo de 72 d.C. e foi com latim desta maneira por sua associação com o nome da família (Fla...</Text>
+                  </Box>
+                </Box>
+                <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+                  <Box>
+                    <Image source={require('../../Assets/grecia.png')} style={styleHomePage.trailPlayImage}/>
+                    <Box style={styleHomePage.containerPlayTrack}>
+                      <Text style={styleHomePage.textPlayTrack}>Formação{'\n'}da Grecia</Text>
+                      <Box style={styleHomePage.containerButtonPlayTrack}>
+                        <Text style={styleHomePage.textbuttonPlayTrack}>Assistir</Text>
+                      </Box>
+                    </Box>
+                  </Box>
+                  <Box>
+                    <Image source={require('../../Assets/imagePaises.png')} style={styleHomePage.trailPlayImage}/>
+                    <Box style={styleHomePage.containerPlayTrack}>
+                      <Text style={styleHomePage.textPlayTrack}>Formação{'\n'}da Grecia</Text>
+                      <Box style={styleHomePage.containerButtonPlayTrack}>
+                        <Text style={styleHomePage.textbuttonPlayTrack}>Assistir</Text>
+                      </Box>
+                    </Box>
+                  </Box>
+                  <Box>
+                    <Image source={require('../../Assets/grecia.png')} style={styleHomePage.trailPlayImage}/>
+                    <Box style={styleHomePage.containerPlayTrack}>
+                      <Text style={styleHomePage.textPlayTrack}>Formação{'\n'}da Grecia</Text>
+                      <Box style={styleHomePage.containerButtonPlayTrack}>
+                        <Text style={styleHomePage.textbuttonPlayTrack}>Assistir</Text>
+                      </Box>
+                    </Box>
+                  </Box>
+                  <Box>
+                    <Image source={require('../../Assets/imagePaises.png')} style={styleHomePage.trailPlayImage}/>
+                    <Box style={styleHomePage.containerPlayTrack}>
+                      <Text style={styleHomePage.textPlayTrack}>Formação{'\n'}da Grecia</Text>
+                      <Box style={styleHomePage.containerButtonPlayTrack}>
+                        <Text style={styleHomePage.textbuttonPlayTrack}>Assistir</Text>
+                      </Box>
+                    </Box>
+                  </Box>
+                </ScrollView>
+              </Box>
+              <Box style={styleHomePage.cardtrail}>
+                <Box style={styleHomePage.containerTrackTrail}>
+                  <Text style={styleHomePage.nameTrail}>Title Track Here</Text>
+                  <Box style={styleHomePage.userTrack}>
+                    <Box style={styleHomePage.photoProfile}></Box>
+                    <Text style={styleHomePage.nameUser}>@nick_name_here</Text>
+                  </Box>
+                  <Box style={styleHomePage.iconArrowTrail}><Icon name="hand-o-right" size={20} /></Box>
+                </Box>
+                <Box style={styleHomePage.containerAboutTrail}>
+                  <Box>
+                    <Text style={styleHomePage.aboutTitleName}>Coliseu</Text>
+                    <Icon name="file-text-o" size={17} style={styleHomePage.iconFIleAbout}/>
+                    <Icon name="circle" style={{fontSize: 6, top: -10, color: '#0B2C37BF'}} /><Text style={styleHomePage.listAbout}>Cretense e Micênicos</Text>
+                    <Icon name="circle" style={{fontSize: 6, top: -10, color: '#0B2C37BF'}} /><Text style={styleHomePage.listAbout}>Os povos gregos</Text>
+                    <Text style={styleHomePage.sumaryAbout}>A construção começou sob o governo de 72 d.C. e foi com latim desta maneira por sua associação com o nome da família (Fla...</Text>
+                  </Box>
+                </Box>
+                <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+                  <Box>
+                    <Image source={require('../../Assets/grecia.png')} style={styleHomePage.trailPlayImage}/>
+                    <Box style={styleHomePage.containerPlayTrack}>
+                      <Text style={styleHomePage.textPlayTrack}>Formação{'\n'}da Grecia</Text>
+                      <Box style={styleHomePage.containerButtonPlayTrack}>
+                        <Text style={styleHomePage.textbuttonPlayTrack}>Assistir</Text>
+                      </Box>
+                    </Box>
+                  </Box>
+                  <Box>
+                    <Image source={require('../../Assets/imagePaises.png')} style={styleHomePage.trailPlayImage}/>
+                    <Box style={styleHomePage.containerPlayTrack}>
+                      <Text style={styleHomePage.textPlayTrack}>Formação{'\n'}da Grecia</Text>
+                      <Box style={styleHomePage.containerButtonPlayTrack}>
+                        <Text style={styleHomePage.textbuttonPlayTrack}>Assistir</Text>
+                      </Box>
+                    </Box>
+                  </Box>
+                  <Box>
+                    <Image source={require('../../Assets/grecia.png')} style={styleHomePage.trailPlayImage}/>
+                    <Box style={styleHomePage.containerPlayTrack}>
+                      <Text style={styleHomePage.textPlayTrack}>Formação{'\n'}da Grecia</Text>
+                      <Box style={styleHomePage.containerButtonPlayTrack}>
+                        <Text style={styleHomePage.textbuttonPlayTrack}>Assistir</Text>
+                      </Box>
+                    </Box>
+                  </Box>
+                  <Box>
+                    <Image source={require('../../Assets/imagePaises.png')} style={styleHomePage.trailPlayImage}/>
+                    <Box style={styleHomePage.containerPlayTrack}>
+                      <Text style={styleHomePage.textPlayTrack}>Formação{'\n'}da Grecia</Text>
+                      <Box style={styleHomePage.containerButtonPlayTrack}>
+                        <Text style={styleHomePage.textbuttonPlayTrack}>Assistir</Text>
+                      </Box>
+                    </Box>
+                  </Box>
+                </ScrollView>
+              </Box>
             </Box>
           </ScrollView>
         </Box>
@@ -136,7 +244,7 @@ export default function HomePage() {
               onPress={() => {
                 pop === false ? popIn() : popOut()
               }}
-            >
+              >
               <Icon name="plus" size={20}/>
             </Pressable>
           </Box>
