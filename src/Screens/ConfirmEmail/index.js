8fb7d8646a0e35e5dc2681e9abb8fdf92e06f098
Brian Lucca <brianlucca2002@gmail.com>
2024-02-08 02:14:05 -0300

    fix: STASTU-106 added scrollview and reorganized the code.

--- src/Screens/ConfirmEmail/index.js
+++ src/Screens/ConfirmEmail/index.js
@@ -8,6 +8,7 @@ import {
   Spinner,
   FormControl,
   useToast,
+  ScrollView,
 } from "native-base";
 import { useNavigation } from "@react-navigation/native";
 import Icon from "react-native-vector-icons/FontAwesome";
@@ -50,70 +51,87 @@ export default function ConfirmEmail() {
   };
 
   return (
-    <Box flex={1} bg="#2B85A2" alignItems="center" justifyContent="center">
-      <Box style={styleConfirmEmail.containerLogo}>
-        <Image source={require("../../Assets/BrandTest2.png")}></Image>
-      </Box>
-      <Box  style={styleConfirmEmail.containerTitle}>
-        <Text style={styleConfirmEmail.titleOne}>Opa!</Text>
-        <Text style={styleConfirmEmail.titleTwo}>Precisamos saber qual e-mail está utilizando, vamos envia-lo um código pra você!</Text>
-      </Box>
-      <Box style={styleConfirmEmail.container}>
-        <Formik
-          initialValues={{ email: "" }}
-          onSubmit={submitCode}
-          validationSchema={yup.object().shape({
-            email: yup.string(),
-          })}
-        >
-          {({ handleChange, setFieldTouched, isValid, values }) => (
-            <FormControl isInvalid={!isValid} style={styleConfirmEmail.formControl}>
-              <FormControl.Label><Text style={styleConfirmEmail.labelInput}>Email or Nickname</Text></FormControl.Label>
-              <Input
-                value={values.email}
-                onChangeText={(text) => {
-                  handleChange("email")(text);
-                  setFieldTouched("email", true);
-                }}
-                onBlur={() => setFieldTouched("email")}
-                style={styleConfirmEmail.input}
-                width={368}
-                borderRadius={8}
-                backgroundColor={'#fff'}
-                variant="filled"
-                placeholder="Type your e-mail or nickname"
-                InputLeftElement={
-                  <Icon
-                    name="at"
-                    size={20}
-                    color="#323232"
-                    style={{ marginLeft: 12, marginRight: 12 }}
+    <Box style={styleConfirmEmail.containerP}>
+      <Box style={styleConfirmEmail.containerSecundario}>
+        <Box style={styleConfirmEmail.containerLogo}>
+          <Image source={require("../../Assets/BrandTest2.png")}></Image>
+        </Box>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <Box style={styleConfirmEmail.containerTitle}>
+            <Text style={styleConfirmEmail.titleOne}>Opa!</Text>
+            <Text style={styleConfirmEmail.titleTwo}>
+              Precisamos saber qual e-mail está utilizando, vamos envia-lo um
+              código pra você!
+            </Text>
+          </Box>
+          <Box style={styleConfirmEmail.container}>
+            <Formik
+              initialValues={{ email: "" }}
+              onSubmit={submitCode}
+              validationSchema={yup.object().shape({
+                email: yup.string(),
+              })}
+            >
+              {({ handleChange, setFieldTouched, isValid, values }) => (
+                <FormControl
+                  isInvalid={!isValid}
+                  style={styleConfirmEmail.formControl}
+                >
+                  <FormControl.Label>
+                    <Text style={styleConfirmEmail.labelInput}>
+                      Email or Nickname
+                    </Text>
+                  </FormControl.Label>
+                  <Input
+                    value={values.email}
+                    onChangeText={(text) => {
+                      handleChange("email")(text);
+                      setFieldTouched("email", true);
+                    }}
+                    onBlur={() => setFieldTouched("email")}
+                    style={styleConfirmEmail.input}
+                    width={"95%"}
+                    borderRadius={8}
+                    backgroundColor={"#fff"}
+                    variant="filled"
+                    placeholder="Type your e-mail or nickname"
+                    InputLeftElement={
+                      <Icon
+                        name="at"
+                        size={20}
+                        color="#323232"
+                        style={{ marginLeft: 12, marginRight: 12 }}
+                      />
+                    }
                   />
-                }
-              />
-            </FormControl>
-          )}
-        </Formik>
+                </FormControl>
+              )}
+            </Formik>
 
-        <Link
-          style={styleConfirmEmail.linkSignIn}
-          href="#"
-          onPress={() => goToScreen()}
-        >
-          <Text style={styleConfirmEmail.textLink}>
-            Don't have an account, Sign Up!
-          </Text>
-        </Link>
-      </Box>
+            <Link
+              style={styleConfirmEmail.linkSignIn}
+              href="#"
+              onPress={() => goToScreen()}
+            >
+              <Text style={styleConfirmEmail.textLink}>
+                Don't have an account, Sign Up!
+              </Text>
+            </Link>
+          </Box>
 
-      <Box style={styleConfirmEmail.botao}>
-        <Button onPress={() => submitCode()} style={styleConfirmEmail.button}>
-          {loading ? (
-            <Spinner color={"cyan.500"} />
-          ) : (
-            <Text style={styleConfirmEmail.textButton}>SEND</Text>
-          )}
-        </Button>
+          <Box style={styleConfirmEmail.boxBotton}>
+            <Button
+              onPress={() => submitCode()}
+              style={styleConfirmEmail.button}
+            >
+              {loading ? (
+                <Spinner color={"cyan.500"} />
+              ) : (
+                <Text style={styleConfirmEmail.textButton}>SEND</Text>
+              )}
+            </Button>
+          </Box>
+        </ScrollView>
       </Box>
     </Box>
   );
