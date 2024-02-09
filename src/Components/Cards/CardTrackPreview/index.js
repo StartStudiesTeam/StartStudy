4ace28521bb8d050fd62fd2d02362c27981e47b6
samuel-jesus <samuel.jesan2018@gmail.com>
2024-02-09 05:03:22 -0300

    feat: added data props using state of TrackPreview Store

--- src/Components/Cards/CardTrackPreview/index.js
+++ src/Components/Cards/CardTrackPreview/index.js
@@ -1,4 +1,4 @@
-import { Box, View, Image, ScrollView, Text } from 'native-base';
+import { Box, View } from 'native-base';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import Header from '../../Cards/Header';
 import Note from '../../Cards/CardTrackPreview/Note';
@@ -6,16 +6,16 @@ import TrackCarousel from '../../TrackCarousel';
 
 import styles from './style';
 
-export default function CardTrackPreview() {
+export default function CardTrackPreview(props) {
     return (
         <View style={styles.container}>
             <Box style={styles.header}>
-                <Header />
+                <Header title={props.titleTrack} profile={props.userProfile} />
                 <Icon name='angle-right' size={24} style={styles.icon} color='#0B2C37' />
             </Box>
-            <Note />
+            <Note note={props.note} titleNote={props.titleNote} />
             <Box>
-                <TrackCarousel />
+                <TrackCarousel index={props.index} items={props.itemsTrack} />
             </Box>
         </View>
     );
