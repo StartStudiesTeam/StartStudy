cdb4b64ea7a385c2faa72221d201bbb9195d3755
samuel-jesus <samuel.jesan2018@gmail.com>
2024-02-09 05:08:19 -0300

    feat: added data props using state of TrackPreview Store, and function for update view notes by item

--- src/Components/TrackCarousel/style.js
+++ src/Components/TrackCarousel/style.js
@@ -30,7 +30,7 @@ const styleTrackCarousel = StyleSheet.create({
         justifyContent: 'center',
         width: '100%',
         height: '100%',
-        opacity: 0.12
+        opacity: 0.16
     },
     title: {
         textAlign: 'center',
