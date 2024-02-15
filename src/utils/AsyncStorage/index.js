98ee5e11e3522857d520343de0ddbbb688da500b
samuel-jesus <samuel.jesan2018@gmail.com>
2024-02-15 02:31:34 -0300

    feat: created method for remove item in Storage and corrected code.

--- src/utils/AsyncStorage/index.js
+++ src/utils/AsyncStorage/index.js
@@ -1,11 +1,13 @@
 import * as SecureStore from 'expo-secure-store';
 
 export const SaveStorage = async (key, value) => {
-    return SecureStore.setItemAsync(key, value);
+    return await SecureStore.setItemAsync(key, value);
 }
 
 export const GetStorage = async (key) => {
-    const value = await SecureStore.getItemAsync(key);
+    return await SecureStore.getItemAsync(key);;
+}
 
-    return value;
+export const RemoveStorage = async (key) => {
+    return await SecureStore.deleteItemAsync(key);
 }
