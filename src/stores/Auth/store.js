3da33de53f3b86b7b11d4eb7ada5a65d845c7f1b
samuel-jesus <samuel.jesan2018@gmail.com>
2024-02-15 02:36:47 -0300

    feat: created states for auth and added in storage.

---
+++ src/stores/Auth/store.js
@@ -0,0 +1,78 @@
+import { create } from 'zustand';
+import api from '../../Services/api';
+import { RemoveStorage, SaveStorage } from '../../utils/AsyncStorage';
+
+export const AuthStore = create((set) => ({
+    user: {},
+    accessToken: null,
+    refreshToken: null,
+    setToken: async (accessToken) => {
+        set({ accessToken });
+        await SaveStorage("accessToken", accessToken);
+    },
+    setRefreshToken: async (refreshToken) => {
+        set({ refreshToken });
+        await SaveStorage("refreshToken", refreshToken);
+    },
+    signOut: async () => {
+        set({
+            accessToken: null,
+            refreshToken: null,
+            user: null,
+        });
+        await RemoveStorage("accessToken");
+        await RemoveStorage("refreshToken");
+        await RemoveStorage("user");
+    },
+    signIn: async (credential) => {
+        try {
+            const response = await api.post("/signin", {
+                email: credential.email,
+                password: credential.password,
+            });
+
+            if (response.body) {
+                const refreshToken = response.body.refreshToken.id;
+                const accessToken = response.body.accessToken;
+                const user = response.body.refreshToken.usersId;
+
+                set({
+                    accessToken: accessToken,
+                    refreshToken: refreshToken,
+                    user: user
+                });
+
+                await SaveStorage("accessToken", accessToken);
+                await SaveStorage("refreshToken", refreshToken);
+                await SaveStorage("user", user);
+            }
+
+            return response;
+
+        } catch (error) {
+            return error.message;
+        }
+    },
+    signUp: async ({ data }) => {
+        try {
+            const response = await api.post("/signup", {
+                name: data.name,
+                nickName: data.nickName,
+                email: data.email,
+                phoneNumber: data.phoneNumber,
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    recoveryPassword: async (data) => {
+        try {
+            const response = await api.post("/forgetpassword", {
+                email: data.email,
+                newPassword: data.newPassword,
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}));
