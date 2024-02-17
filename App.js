429fab99809a07c8dd402965cdb720968bb84c1b
samuel-jesus <samuel.jesan2018@gmail.com>
2024-02-16 22:02:53 -0300

    feat: added loading app with Splash and added state in app

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-// import Routes from './src/Routes';
+import Routes from './src/Routes';
 import MenuTabBottomNavigator from './src/Components/MenuTabBottomNavigator';
 import { NativeBaseProvider } from "native-base";
 import {
@@ -11,35 +11,36 @@ import {
   Ubuntu_400Regular_Italic,
   Ubuntu_500Medium,
   Ubuntu_500Medium_Italic,
-  Ubuntu_600SemiBold,
   Ubuntu_700Bold,
   Ubuntu_700Bold_Italic
 } from "@expo-google-fonts/ubuntu";
 import AppLoading from "expo-app-loading";
+import SplashScreen from 'expo-splash-screen';
+import { AuthStore } from './src/stores/Auth/store';
 
 export default function App() {
+  const user = AuthStore((state) => state.user);
 
-  const [isFontLoaded] = useFonts({
+  const [isFontLoaded, error] = useFonts({
     Ubuntu_300Light,
     Ubuntu_300Light_Italic,
     Ubuntu_400Regular,
     Ubuntu_400Regular_Italic,
     Ubuntu_500Medium,
     Ubuntu_500Medium_Italic,
-    Ubuntu_600SemiBold,
     Ubuntu_700Bold,
     Ubuntu_700Bold_Italic,
-
   });
 
   if (!isFontLoaded) {
-    <AppLoading />
+    return <AppLoading />;
   }
+
 
   return (
     <NativeBaseProvider>
       <NavigationContainer>
-        <MenuTabBottomNavigator />
+        <Routes />
       </NavigationContainer>
     </NativeBaseProvider>
   );
