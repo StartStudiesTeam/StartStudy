ccfe7e6f0a4b715b0eff851cf4ecde8e86d7b483
samuel-jesus <samuel.jesan2018@gmail.com>
2024-02-28 04:23:30 -0300

    fix: corrected EmailConfirm for user function in state story and navigate for screen correct

--- src/Screens/ConfirmEmail/index.js
+++ src/Screens/ConfirmEmail/index.js
@@ -4,7 +4,6 @@ import {
   Box,
   Text,
   Button,
-  Link,
   Spinner,
   FormControl,
   useToast,
@@ -14,42 +13,34 @@ import { useNavigation } from "@react-navigation/native";
 import Icon from "react-native-vector-icons/FontAwesome";
 import { Image } from "react-native";
 import { Formik } from "formik";
-import styleConfirmEmail from "./styles";
-import api from "../../Services/api";
-import validationForm from "./schemaValidation";
 import { TouchableOpacity } from "react-native";
+import styleConfirmEmail from "./styles";
+import validationForm from "./schemaValidation";
+import { AuthStore } from "../../stores/Auth/store";
 
 
 export default function ConfirmEmail() {
+  const { mailCheck } = AuthStore();
   const { navigate, goBack } = useNavigation();
-  const [loading, setIsLoanding] = useState(false);
+  const [loading, setIsLoading] = useState(false);
   const toast = useToast();
 
-  async function goToScreen() {
-    navigate("SignUp");
-  }
+  const handleSubmit = async (formValues) => {
+    setIsLoading(true);
 
-  const handleSubmit = async (values) => {
-    try {
-      setIsLoanding(true);
-      const response = await api.get("MailCheck", {
-        email: values.email,
-      });
-      toast.show({
-        description: `Verifique seu Email`,
-      });
-      const confirmationCode = response.confirmationCode;
-      await sendEmail(values.email, confirmationCode);
-      setTimeout(() => {
-        setIsLoanding(false);
-        navigate("CodeConfirm");
-      }, 2000);
-    } catch (error) {
-      toast.show({
-        description: `${error.message}`,
-      });
-      setIsLoanding(false);
+    const credentialEmail = formValues.email;
+
+    const response = await mailCheck({ email: credentialEmail });
+
+    toast.show({
+      description: `${response.message}`,
+    });
+
+    if (response?.statusCode == 200) {
+      navigate("CodeConfirm");
     }
+
+    setIsLoading(false);
   };
 
   return (
@@ -57,18 +48,18 @@ export default function ConfirmEmail() {
       <Box style={styleConfirmEmail.box}>
         <ScrollView showsVerticalScrollIndicator={false}>
           <Formik
-                initialValues={{email: ''}}
-                onSubmit={handleSubmit}
-                validationSchema={validationForm}
+            initialValues={{ email: '' }}
+            onSubmit={handleSubmit}
+            validationSchema={validationForm}
           >
-            {({ handleChange, handleSubmit, setFieldTouched, touched, errors, values}) => (
+            {({ handleChange, handleSubmit, setFieldTouched, touched, errors, values }) => (
               <>
                 <Box style={styleConfirmEmail.containerBrandImage}>
                   <Image source={require('../../Assets/BrandTest2.png')} />
                 </Box>
                 <Box style={styleConfirmEmail.contentHeaderMessage}>
                   <Text style={styleConfirmEmail.headerTitleWelcome}>Opa!</Text>
-                  <Text style={styleConfirmEmail.headerTitleWelcomeSub}>Precisamos saber qual e-mail está utilizando, vamos envia-lo um codigo pra você!</Text>
+                  <Text style={styleConfirmEmail.headerTitleWelcomeSub}>Precisamos saber qual e-mail está utilizando, vamos envia-lo um código para confirmação!</Text>
                 </Box>
                 <Box style={styleConfirmEmail.content}>
                   <Box style={styleConfirmEmail.formContent}>
@@ -76,26 +67,26 @@ export default function ConfirmEmail() {
                       <FormControl isInvalid={errors.email && touched.email} style={styleConfirmEmail.formControl}>
                         <Text style={styleConfirmEmail.textLabel}>Email or Nickname</Text>
                         <Input
-                              style={styleConfirmEmail.input}
-                              variant='filled'
-                              borderRadius={8}
-                              backgroundColor='#fff'
-                              placeholder="Type your Email"
-                              values={values.email}
-                              onChangeText={(text) => {
-                                handleChange('email')(text)
-                              }}
-                              onBlur={() => setFieldTouched('email')}
-                              InputLeftElement={
-                                <Icon name="at" style={styleConfirmEmail.iconInputLeft}/>
-                              }    
+                          style={styleConfirmEmail.input}
+                          variant='filled'
+                          borderRadius={8}
+                          backgroundColor='#fff'
+                          placeholder="Type your Email"
+                          values={values.email}
+                          onChangeText={(text) => {
+                            handleChange('email')(text)
+                          }}
+                          onBlur={() => setFieldTouched('email')}
+                          InputLeftElement={
+                            <Icon name="at" style={styleConfirmEmail.iconInputLeft} />
+                          }
                         />
                         <FormControl.ErrorMessage>
-                              {errors.email}
-                        </FormControl.ErrorMessage>  
+                          {errors.email}
+                        </FormControl.ErrorMessage>
                       </FormControl>
                       <Box style={styleConfirmEmail.containerLink}>
-                        <TouchableOpacity onPress={() => goToScreen()}>
+                        <TouchableOpacity onPress={() => navigate("SignUp")}>
                           <Text style={styleConfirmEmail.labelLink}>Don't have an account, sign up?</Text>
                         </TouchableOpacity>
                       </Box>
