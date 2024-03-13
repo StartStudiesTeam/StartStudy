10a3103f9d3c3510cc1a96575c66a62faba7377e
samuel-jesus <samuel.jesan2018@gmail.com>
2024-03-12 22:25:39 -0300

    fix: added method for use api and refactor form

--- src/Screens/CodeConfirm/index.js
+++ src/Screens/CodeConfirm/index.js
@@ -5,35 +5,33 @@ import {
   Box,
   Text,
   Button,
-  Link,
   Spinner,
   useToast,
   ScrollView,
 } from "native-base";
 import { useNavigation } from "@react-navigation/native";
+import { Formik } from "formik";
 import styleCodeConfirm from "./styles";
 import { AuthStore } from "../../stores/Auth/store";
 
 export default function CodeConfirm() {
+  const pageFlow = AuthStore((state) => state.pageFlow);
+  const { confirmCodeToken } = AuthStore();
   const { navigate, goBack } = useNavigation();
   const [loading, setIsLoading] = useState(false);
   const toast = useToast();
-  const data = [];
+  const inputRefs = Array(6).fill().map(() => useRef(null));
 
-  const inputRefs = Array(6)
-  .fill()
-  .map(() => useRef(null));
-
-
-  const submitCode = async () => {
+  const handleSubmit = async (formValues) => {
     setIsLoading(true);
 
-    const codeToken = data.join("");
+    const codeToken = Object.values(formValues).join('');
 
     const response = await confirmCodeToken({
       code: codeToken,
-      email: user.email,
     });
+
+    console.log(response)
 
     toast.show({
       description: `${response.message}`,
@@ -50,32 +48,15 @@ export default function CodeConfirm() {
     setIsLoading(false);
   };
 
-  let inputComponents = [];
-  for (let index = 0; index <= 5; index++) {
-    inputComponents.push(
-      <Input
-        key={index}
-        ref={inputRefs[index]}
-        style={styleCodeConfirm.input}
-        variant="filled"
-        backgroundColor={"#FFF"}
-        width="47px"
-        keyboardType="default"
-        maxLength={1}
-        autoFocus={index === 0}
-        onChangeText={(text) => {
-          data[index] = text;
-          if (text === "") {
-            if (index > 0) {
-              inputRefs[index - 1].current.focus();
-            }
-          } else if (text !== "" && index < 5) {
-            inputRefs[index + 1].current.focus();
-          }
-        }}
-      />
-    );
-  }
+  handleInputs = (text, index) => {
+    if (text === "") {
+      if (index > 0) {
+        inputRefs[index - 1].current.focus();
+      }
+    } else if (text !== "" && index < 5) {
+      inputRefs[index + 1].current.focus();
+    }
+  };
 
   return (
     <Box style={styleCodeConfirm.container}>
@@ -84,32 +65,55 @@ export default function CodeConfirm() {
           <Box style={styleCodeConfirm.containerBrandImage}>
             <Image source={require('../../Assets/BrandTest2.png')} style={styleCodeConfirm.brandImage} />
           </Box>
-          <Box style={styleCodeConfirm.contentHeaderMessage}>
-            <Text style={styleCodeConfirm.headerTitleWelcome}>Você é você mesmo?!</Text>
-            <Text style={styleCodeConfirm.headerTitleWelcomeSub}>Precisamos confirma se você recebeu o código no seu e-mail informado!</Text>
-          </Box>
-          <Box style={styleCodeConfirm.content}>
-            <Box style={styleCodeConfirm.codeContent}>
-              <Box style={styleCodeConfirm.boxCode}>
-                <Text style={styleCodeConfirm.textLabel}>Code</Text>
-                <Box style={styleCodeConfirm.code}>
-                  <>{inputComponents}</>
+          <Formik initialValues={{}} onSubmit={handleSubmit}>
+            {({ handleChange, handleSubmit, setFieldTouched, touched, errors, values }) => (
+              <>
+                <Box style={styleCodeConfirm.contentHeaderMessage}>
+                  <Text style={styleCodeConfirm.headerTitleWelcome}>Você é você mesmo?!</Text>
+                  <Text style={styleCodeConfirm.headerTitleWelcomeSub}>Precisamos confirmar se você recebeu o código no e-mail informado!</Text>
                 </Box>
-                <Box style={styleCodeConfirm.containerLink}>
-                  <TouchableOpacity onPress={() => goToSignUp()}>
-                    <Text style={styleCodeConfirm.labelLink}>Don't have an account? signup?</Text>
-                  </TouchableOpacity>
+                <Box style={styleCodeConfirm.content}>
+                  <Box style={styleCodeConfirm.codeContent}>
+                    <Box style={styleCodeConfirm.boxCode}>
+                      <Text style={styleCodeConfirm.textLabel}>Code</Text>
+                      <Box style={styleCodeConfirm.code}>
+                        {[...Array(6)].map((_, index) => (
+                          <Input
+                            key={index}
+                            ref={inputRefs[index]}
+                            style={styleCodeConfirm.input}
+                            variant="filled"
+                            backgroundColor={"#FFF"}
+                            width="44px"
+                            keyboardType="default"
+                            maxLength={1}
+                            autoFocus={index === 0}
+                            value={values[`code${index}`]}
+                            onChangeText={(text) => {
+                              handleChange(`code${index}`)(text);
+                              handleInputs(text, index);
+                            }}
+                          />
+                        ))}
+                      </Box>
+                      <Box style={styleCodeConfirm.containerLink}>
+                        <TouchableOpacity onPress={() => navigate('SignUp')}>
+                          <Text style={styleCodeConfirm.labelLink}>Don't have an account? signup?</Text>
+                        </TouchableOpacity>
+                      </Box>
+                    </Box>
+                  </Box>
+                  <Button onPress={() => handleSubmit()} style={styleCodeConfirm.button}>
+                    {loading ? (
+                      <Spinner color={'cyan.500'} />
+                    ) : (
+                      <Text style={styleCodeConfirm.labelButton}>SIGN IN</Text>
+                    )}
+                  </Button>
                 </Box>
-              </Box>
-            </Box>
-            <Button onPress={submitCode} style={styleCodeConfirm.button}>
-              {loading ? (
-                <Spinner color={'cyan.500'} />
-              ) : (
-                <Text style={styleCodeConfirm.labelButton}>SIGN IN</Text>
-              ) }
-            </Button>
-          </Box>
+              </>
+            )}
+          </Formik>
         </ScrollView>
       </Box>
     </Box>
