657d0ec6b7af982260e761851c2c220a80b70964
Brianlucca <brianlucca2002@gmail.com>
2024-03-27 22:40:50 -0300

    feat: STASTU-207 added style to the register roadmap, added the container for the flex and the background color of the screen.

---
+++ src/Screens/Roadmap/Register/style.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+
+
+
+const styleRegisterRoad = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#B1D5DE',
+  },
+  });
+  
+  export default styleRegisterRoad;
+  
