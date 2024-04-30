1189e6f049cd78cee5a0f2cd04dfaa54a86a0ab6
samuel-jesus <samuel.jesan2018@gmail.com>
2024-04-30 06:51:25 -0300

    feat: added util for select font style

---
+++ src/utils/Fonts/fonts.js
@@ -0,0 +1,12 @@
+const font = {
+    "light": "Ubuntu_300Light",
+    "regular": "Ubuntu_400Regular",
+    "medium": "Ubuntu_500Medium",
+    "bold": "Ubuntu_700Bold",
+    "lightItalic": "Ubuntu_300Light_Italic",
+    "regularItalic": "Ubuntu_400Regular_Italic",
+    "mediumItalic": "Ubuntu_500Medium_Italic",
+    "boldItalic": "Ubuntu_700Bold_Italic"
+}
+
+export { font }
