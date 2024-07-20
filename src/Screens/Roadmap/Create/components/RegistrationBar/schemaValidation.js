09b9f7a3819498ec60c6940858f3627379db6b1e
samuel-jesus <samuel.jesan2018@gmail.com>
2024-07-20 03:35:39 -0300

    feat: created component for register new roadmaps and videos, with forms, validation and style

---
+++ src/Screens/Roadmap/Create/components/RegistrationBar/schemaValidation.js
@@ -0,0 +1,17 @@
+import * as yup from 'yup'
+const validationForm = () => {
+    return (
+        yup.object().shape({
+            title: yup
+                .string()
+                .required('The title is required!'),
+            description: yup
+                .string()
+                .required('Description is required!'),
+            tags: yup
+                .string()
+        })
+    )
+}
+
+export default validationForm;
---
+++ src/Screens/Roadmap/Create/components/RegistrationBar/style.js
@@ -0,0 +1,83 @@
+import { StyleSheet } from "react-native";
+import { font } from "../../../../../utils/Fonts/fonts";
+
+const styleRegistration = StyleSheet.create({
+    container: {
+        gap: 16,
+    },
+    containerInput: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "flex-start",
+        gap: 8,
+        alignSelf: "stretch",
+        paddingHorizontal: 16,
+    },
+    labelInput: {
+        fontFamily: font.regular,
+        alignSelf: "stretch",
+        fontSize: 16,
+        fontWeight: 300,
+        lineHeight: 16,
+        color: "#FFFFFF",
+    },
+    input: {
+        display: "flex",
+        height: 48,
+        padding: 12,
+        gap: 12,
+        fontSize: 16,
+        alignItems: "flex-start",
+        alignSelf: "stretch",
+        fontFamily: font.regular,
+        color: "#00000080",
+    },
+    iconInput: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: 14,
+        color: '#ccc',
+        paddingHorizontal: 12,
+    },
+    iconInputLeft: {
+        color: "#ccc",
+        marginLeft: 12,
+    },
+    containerTextArea: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "flex-start",
+        gap: 8,
+        alignSelf: "stretch",
+        paddingHorizontal: 16,
+    },
+    labelTextArea: {
+        fontFamily: font.regular,
+        alignSelf: "stretch",
+        fontSize: 16,
+        fontWeight: 300,
+        lineHeight: 16,
+        color: "#FFFFFF",
+    },
+    textArea: {
+        fontFamily: font.regular,
+        color: "#00000080",
+        fontSize: 14,
+    },
+    containerButton: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "flex-end",
+        alignItems: "center",
+        alignSelf: "stretch",
+        paddingHorizontal: 16,
+        marginVertical: 8,
+    },
+    button: {
+        backgroundColor: "#77ADCB",
+        display: "flex",
+        width: 90,
+    },
+});
+export default styleRegistration;
