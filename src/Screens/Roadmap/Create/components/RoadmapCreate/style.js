dc05ee2faa8d365bfdc9e129b50444d265ec5b98
samuel-jesus <samuel.jesan2018@gmail.com>
2024-07-20 03:44:25 -0300

    feat: created component for show videos roadmap added

---
+++ src/Screens/Roadmap/Create/components/RoadmapCreate/style.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+const styleCreateRoadmap = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingHorizontal: 16,
+        paddingTop: 32,
+        width: '100%',
+    }
+});
+export default styleCreateRoadmap 
