a6eaacbc90520426c86913877c965801712c7870
samuel-jesus <samuel.jesan2018@gmail.com>
2024-07-20 03:49:56 -0300

    feat: created screen for create roadmap

---
+++ src/Screens/Roadmap/Create/style.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+import { font } from "../../../utils/Fonts/fonts";
+
+
+
+const styleRegisterRoad = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fffa',
+  },
+  title: {
+    alignSelf: 'center',
+    fontFamily: font.medium,
+    color: '#ccc',
+    fontSize: 20,
+    marginBottom: 8,
+    marginTop: 16,
+  }
+});
+
+export default styleRegisterRoad;
