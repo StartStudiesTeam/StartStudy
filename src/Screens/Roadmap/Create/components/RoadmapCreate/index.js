cdb614f1c22cc6fe0ff60714e0b1ca3526c6ec3a
samuel-jesus <samuel.jesan2018@gmail.com>
2024-09-12 02:01:05 -0300

    feat: added component for timeline track videos and get current roadmap selected

--- src/Screens/Roadmap/Create/components/RoadmapCreate/index.js
+++ src/Screens/Roadmap/Create/components/RoadmapCreate/index.js
@@ -1,22 +1,48 @@
-import React from "react";
-import { View } from 'native-base';
+import React, { useEffect, useState } from "react";
+import { Text, View } from 'native-base';
 import CardCreateRoadmap from "../CardRoadmap";
 import styleCreateRoadmap from "./style";
-
+import { RoadmapStore } from "../../../../../stores/Roadpmap/store";
 export default TimelineRoadmap = () => {
+    const { getCurrentRoadmapId, getRoadmap } = RoadmapStore();
+    const [videosRoadmap, setVideosRoadmap] = useState(null);
+    const [error, setError] = useState(null);
+
+
+    useEffect(() => {
+        const fetchRoadmap = async () => {
+            try {
+                const currentRoadmapId = await getCurrentRoadmapId();
+                const roadmapData = await getRoadmap({ id: currentRoadmapId });
+
+                setVideosRoadmap(roadmapData?.body?.data?.VideosRoadmap);
+            } catch (error) {
+                setError(error);
+                console.error('Erro ao obter o Roadmap:', error);
+            }
+        };
+        fetchRoadmap();
+    }, [getCurrentRoadmapId, getRoadmap]);
+
     return (
         <View style={styleCreateRoadmap.container}>
+            <Text style={styleCreateRoadmap.labelFormVideo}> Adicionar conteúdos </Text>
             <CardCreateRoadmap
                 title="Teste"
                 content="..."
                 index={1}
                 type="create"
             />
-            <CardCreateRoadmap
-                title="Title 2"
-                content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-                index={2}
-            />
+            {
+                videosRoadmap?.map((video, index) => (
+                    <CardCreateRoadmap
+                        key={index}
+                        title={video?.Videos?.title}
+                        content={video?.Videos?.description}
+                        index={index}
+                    />
+                ))
+            }
         </View>
     )
 }
--- src/Screens/Roadmap/Create/components/RoadmapCreate/style.js
+++ src/Screens/Roadmap/Create/components/RoadmapCreate/style.js
@@ -1,11 +1,21 @@
 import { StyleSheet } from "react-native";
+import { font } from "../../../../../utils/Fonts/fonts";
+
 
 const styleCreateRoadmap = StyleSheet.create({
     container: {
         flex: 1,
         paddingHorizontal: 16,
         paddingTop: 32,
+        marginBottom: 64,
         width: '100%',
-    }
+    },
+    labelFormVideo: {
+        textAlign: "center",
+        fontFamily: font.medium,
+        color: "#595959",
+        fontWeight: 400,
+        fontSize: 16,
+    },
 });
 export default styleCreateRoadmap 
