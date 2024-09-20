756ecdb1569969718fa205e6aff11cc89e8971a1
samuel-jesus <samuel.jesan2018@gmail.com>
2024-09-20 04:49:30 -0300

    feat: created component buttons filters roadmap viewer

---
+++ src/Screens/Roadmap/Viewer/components/FiltersButton/style.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+const styleFiltersButton = StyleSheet.create({
+  buttonFilter: {
+    borderRadius: 8,
+    borderColor: "#549DB4",
+    justifyContent: "center",
+    alignItems: "center",
+    height: 28,
+  },
+});
+
+export default styleFiltersButton;
